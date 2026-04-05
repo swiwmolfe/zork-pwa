8fa3180ad90b3ea13f42c3480eb4be56c23f5e61
TokenLiquidity <124472084+TokenLiquidity@users.noreply.github.com>
2026-04-04 21:45:07 -0400

    Zork I PWA - initial deploy

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'zork-pwa-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.addAll(['./', 'index.html', 'manifest.json', 'icon-192.svg', 'icon-512.svg', 'zork1.z3'])
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      // Cache new requests dynamically (for Vite-hashed JS/CSS assets)
+      if (response.ok) {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+      }
+      return response;
+    }))
+  );
+});
